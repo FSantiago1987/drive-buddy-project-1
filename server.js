1a2e2dff6e15e68ffa7ad68417bc07715c00cc03
Alfredo Neto <alfredoneto@Alfredos-MacBook-Pro.local>
2021-01-25 14:20:11 -0500

    Seventh Atempt

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require("express");
-const path = require('path');
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
 const app = express();
-const publicPath = path.join(__dirname, 'client', 'build');
-app.use(express.static(publicPath));
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
@@ -31,7 +28,4 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 const port = process.env.PORT || 5000;
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
-});
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
