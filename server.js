d8f2df717880e681072a0dd34b89a8371ac408ed
Alfredo Neto <alfredoneto@Alfredos-MacBook-Pro.local>
2021-01-25 14:07:01 -0500

    Fifth Atempt

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
 const app = express();
-const publicPath = path.join(__dirname, 'client', 'public');
+const publicPath = path.join(__dirname, 'client', 'build');
 app.use(express.static(publicPath));
 // Bodyparser middleware
 app.use(
